e26b0341b8e4d4c8fe61f9e43245d52e9949e91d
Abdul Latif Syafi'i <a.l.syafii@gmail.com>
2017-08-06 23:54:27 +0700

    Add nativebase

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'native-base';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Awesome!</Text>
+        <Button info block><Text>Awesome!</Text></Button>
       </View>
     );
   }
