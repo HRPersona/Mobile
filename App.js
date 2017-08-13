f9496f5b92c899375931c2560b273480cd1dc18f
Abdul Latif Syafi'i <a.l.syafii@gmail.com>
2017-08-14 01:46:01 +0700

    Move register app from node_modules to App.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'native-base';
 import Login from './src/screens/Login';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (<Login />);
   }
 }
+
+Expo.registerRootComponent(App);
